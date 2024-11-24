6b46622077faa904350393c70c552c246ec2f803
monusbence <72020098+monusbence@users.noreply.github.com>
2024-11-24 22:08:50 +0100

    Add files via upload

---
+++ Matrix/Matrix/Matrix.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Matrix
+{
+    internal class Matrix
+    {
+        private double[,] elemek;
+
+        public Matrix()
+        {
+            elemek = new double[3, 3];
+        }
+
+        public Matrix(int meret)
+        {
+            elemek = new double[meret, meret];
+        }
+
+        public Matrix(int sorok, int oszlopok)
+        {
+            elemek = new double[sorok, oszlopok];
+        }
+
+        public static Matrix Osszead(Matrix elso, Matrix masodik)
+        {
+            if (elso.SorokSzama != masodik.SorokSzama || elso.OszlopokSzama != masodik.OszlopokSzama)
+            {
+                throw new ArgumentException("A mátrixok méretének egyeznie kell az összeadáshoz!");
+            }
+
+            Matrix eredmeny = new Matrix(elso.SorokSzama, elso.OszlopokSzama);
+
+            for (int sor = 0; sor < elso.SorokSzama; sor++)
+            {
+                for (int oszlop = 0; oszlop < elso.OszlopokSzama; oszlop++)
+                {
+                    eredmeny.elemek[sor, oszlop] = elso.elemek[sor, oszlop] + masodik.elemek[sor, oszlop];
+                }
+            }
+
+            return eredmeny;
+        }
+
+        public void Feltolt(double ertek)
+        {
+            for (int sor = 0; sor < SorokSzama; sor++)
+            {
+                for (int oszlop = 0; oszlop < OszlopokSzama; oszlop++)
+                {
+                    elemek[sor, oszlop] = ertek;
+                }
+            }
+        }
+
+        public int OszlopokSzama
+        {
+            get { return elemek.GetLength(1); }
+        }
+
+        public int SorokSzama
+        {
+            get { return elemek.GetLength(0); }
+        }
+
+        public override string ToString()
+        {
+            string eredmeny = "";
+
+            for (int sor = 0; sor < SorokSzama; sor++)
+            {
+                for (int oszlop = 0; oszlop < OszlopokSzama; oszlop++)
+                {
+                    eredmeny += elemek[sor, oszlop] + " ";
+                }
+                eredmeny += "\n";
+            }
+
+            return eredmeny;
+        }
+    }
+}
---
+++ Matrix/Matrix/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix matrix1 = new Matrix();
+            Matrix matrix2 = new Matrix(3);
+            Matrix matrix3 = new Matrix(3, 3);
+
+            matrix1.Feltolt(1.0);
+            matrix2.Feltolt(2.0);
+            matrix3.Feltolt(3.0);
+
+            Console.WriteLine("Első mátrix:");
+            Console.WriteLine(matrix1);
+
+            Console.WriteLine("Második mátrix:");
+            Console.WriteLine(matrix2);
+
+            Console.WriteLine("Harmadik mátrix:");
+            Console.WriteLine(matrix3);
+
+            
+            Matrix osszegMatrix = Matrix.Osszead(matrix1, matrix2);
+            Console.WriteLine("Első és második mátrix összege:");
+            Console.WriteLine(osszegMatrix);
+            
+            
+                
+        }
+    }
+}
